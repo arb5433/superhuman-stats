9bcddb63613c8de88adb3fea9261b8347741b982
adambailey <arb5433@gmail.com>
2021-07-29 02:58:14 -0400

    “.”

--- src/components/Heros/index.js
+++ src/components/Heros/index.js
@@ -6,25 +6,46 @@ import './Heros.css';
 const Heros = () => {
   const history = useHistory();
   const {universe} = useParams();
+  const [allSuperHumans, setAllSuperHumans] = useState('')
   const [heros, setHeros] = useState('');
+  const [publisher, setPublisher] = useState('');
+  
+  // format the publisher name from the name of the universe to filter results
+  useEffect(() => {
+    const formatted = universe === 'Other' ? 'Other' : `${universe} Comics`;
+    setPublisher(formatted);
+    console.log('useEffect One')
+  },[universe]);
 
+  // fetch all superheros, then filer it down to the appropriate list based on selections
   useEffect(() => {
     const fetchHeros = async() =>{
       const response = await fetch(`https://akabab.github.io/superhero-api/api/all.json`)
-      // console.log(response, 'res');
       const allSupers = await response.json();
-      // console.log(allSupers,'all')
-      setHeros(allSupers)
+      setAllSuperHumans(allSupers);
     }
-    fetchHeros()
-  }, [])
+    console.log('UseEffect 2')
+    if (!allSuperHumans) fetchHeros();
+    if (!heros) {
+      let selectSupers;
+      if (publisher === 'Other'){
+        selectSupers = allSuperHumans ? allSuperHumans.filter(hero => hero.biography.publisher !== 'DC Comics' && hero.biography.publisher !== 'Marvel Comics' && hero.biography.alignment === 'good') : null;
+      } else {
+        selectSupers = allSuperHumans ? allSuperHumans.filter(hero => hero.biography.publisher === publisher && hero.biography.alignment === 'good') : null;
+      }
+      setHeros(selectSupers)
+    }
+  }, [allSuperHumans,publisher, heros])
 
 
   return (
     <div>
-      <div>Heros Tests</div>
+      <div className='alignment'>
+        <div>Hero</div>
+        <div>Villain</div>
+      </div>
       {heros && heros.map(hero => (
-        <div>{hero.id}</div>
+        <div key={hero.id}>{hero.id}</div>
       ))}
     </div>
   )
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,17 +8,22 @@ const Home = () => {
   const history = useHistory();
 
   const marvelClick = () => {
-    history.push('/marvel/heros')
+    history.push('/Marvel/heros');
   }
 
   const dcClick = () => {
-    history.push('/dc/heros')
+    history.push('/DC/heros');
+  }
+
+  const otherClick = () => {
+    history.push('/Other/heros');
   }
 
   return (
     <div className='home-wrapper'>
       <div className='marvel-banner' onClick={marvelClick}>This will be the marvel banner</div>
       <div className='dc-banner' onClick={dcClick}>This will be the dc banner</div>
+      <div className='other-banner' onClick={otherClick}>This will be the other banner</div>
     </div>
   )
 }
